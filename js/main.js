fa68f00a192644293e0f62b6a0cbf6a7968eb306
Edson Pinheiro <edrpj7@gmail.com>
2021-01-26 18:08:14 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+//sobre Hendrix
+function play(){
+     //console.log("clicou")
+     
+    window.open("https://youtu.be/qFfnlYbFEiE");      
+}
+
+function info(){
+    window.open("https://pt.wikipedia.org/wiki/Jimi_Hendrix");
+}
